d7423a606fd454f3b5fbcb0fd8e71c7f3292b077
Jose <josevercas44@gmail.com>
2023-04-12 20:14:37 +0200

    file compilation

---
+++ build/CMakeFiles/vcstatus.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jose/Codigo/.conf/vcstatus/util.cpp" "CMakeFiles/vcstatus.dir/util.cpp.o" "gcc" "CMakeFiles/vcstatus.dir/util.cpp.o.d"
+  "/home/jose/Codigo/.conf/vcstatus/vcstatus.cpp" "CMakeFiles/vcstatus.dir/vcstatus.cpp.o" "gcc" "CMakeFiles/vcstatus.dir/vcstatus.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/vcstatus.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vcstatus.dir/util.cpp.o"
+  "CMakeFiles/vcstatus.dir/util.cpp.o.d"
+  "CMakeFiles/vcstatus.dir/vcstatus.cpp.o"
+  "CMakeFiles/vcstatus.dir/vcstatus.cpp.o.d"
+  "vcstatus"
+  "vcstatus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vcstatus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
